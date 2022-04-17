a8f759571bc8e4ccd347b0be741c94753dedd112
star-min <zxzx00262@gmail.com>
2022-04-17 20:36:23 +0900

    python basic

---
+++ Python/basic/basicEx1.py
@@ -0,0 +1,12 @@
+x,y,z = 10,20,30
+
+print(x,y,z)
+
+q=w=e= 40
+
+print(q,w,e)
+
+z, y = 10, 20
+z, y = y, z
+print(z,y)
+
---
+++ Python/basic/basicEx3.py
@@ -0,0 +1,10 @@
+# x = input()
+# print(x)
+
+# a = input()
+# b = input()
+# print(a,b)
+
+c = int(input())
+d = int(input())
+print(c+d)
