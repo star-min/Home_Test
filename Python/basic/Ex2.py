5281d852865d6cf1b5950abc76284b47829df4e2
star-min <103565981+star-min@users.noreply.github.com>
2022-06-13 16:09:54 +0900

    Add files via upload

---
+++ Python/basic/Ex2.py
@@ -0,0 +1,21 @@
+# 입력 함수
+# x = input()
+# print(x)
+# input 를 이용해 두 숫자의 합
+# a = input('첫 번째 숫자를 입력해 주세요')
+# b = input('두 번째 숫자를 입력해 주세요')
+# print(a + b)
+# 위 처럼하면 문자열로 들어감
+# a = int(input('첫 번째 숫자를 입력해 주세요'))
+# b = int(input('두 번째 숫자를 입력해 주세요'))
+# print(a + b)
+# a, b = input('문자열 두개를 입력').split()
+# print(a)
+# print(b)
+
+# c, d = input('숫자 두 개를 입력').split()
+# c = int(c)
+# d = int(d)
+# print(c+d)
+
+input().split(',') # split에 콤마를 넣으면 콤마를 기준으로 구분하게됨 공백은 띄어쓰기 
